142230793cd8d92ca7f83e7268f3ee6ffc04c49c
Cix <gabrielviniciodacosta@gmail.com>
2023-09-29 09:41:50 -0300

    Professor ruim desligou Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,14 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
+    
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0
   }
 }
